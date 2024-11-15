ec1c451d2cb5f9d8c8fb7c719af88daa51a9728a
Anmol <57932450+ErAnmolgurjar@users.noreply.github.com>
2024-11-15 23:34:56 +0530

    Create editor.cs

---
+++ editor.cs
@@ -0,0 +1,19 @@
+.ql-stroke {
+	fill: none;
+        stroke: #444;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke-width: 2;
+}
+.ql-stroke-miter {
+	fill: none;
+        stroke: #444;
+        stroke-miterlimit: 10;
+        stroke-width: 2;
+}
+.ql-fill,.ql-stroke.ql-fill{
+	fill: #444;
+}
+.ql-thin,.ql-stroke.ql-thin {
+	stroke-width: 1;
+}
